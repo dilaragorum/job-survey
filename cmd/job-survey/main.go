0ca5971b4c205d9c0f13818b650588bcfc80cdbb
dilaragorum <dilaragorum@gmail.com>
2022-10-22 23:00:51 +0300

    rearrange cmd directory v2

--- cmd/job-survey/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/dilaragorum/job-survey/cmd/job-survey/cli"
-)
-
-func main() {
-	cli.Execute()
-}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/dilaragorum/job-survey/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
