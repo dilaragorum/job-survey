f728d15e42cdf1845c4f1b29985275b3e0405137
dilaragorum <dilaragorum@gmail.com>
2022-10-22 22:50:04 +0300

    change main.go directory

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/dilaragorum/job-survey/cmd/job-survey"
+
+func main() {
+	job_survey.Execute()
+}
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/dilaragorum/job-survey/cmd/job-survey"
-)
-
-func main() {
-	job_survey.Execute()
-}
